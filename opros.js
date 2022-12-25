1b0b7d2fed316b9d76a90d162d8b6c22628ce469
Shakhrukh Abdugapar <69681238+shahruhabd@users.noreply.github.com>
2022-12-25 14:22:56 +0600

    Add files via upload

---
+++ opros.js
@@ -0,0 +1,52 @@
+let name = document.querySelector('#name')
+let age = document.querySelector('#age')
+let sbmt = document.querySelector('#submitBTN')
+let block = document.querySelector('#opros-block')
+let error = document.createElement('div');
+let select = document.querySelector("#select")
+let errorAge = document.createElement('div');
+let errorStudent = document.createElement('div');
+let ageBool = false 
+let nameBool = false 
+let studBool = false 
+
+
+sbmt.addEventListener("click", submitForm)
+function checkName() {
+    if (name.value.length < 3) {
+        error.innerHTML = "Вы некорректно ввели имя, должно быть от 3 символов"
+        block.append(error)
+    } else {
+        error.innerHTML = ""
+        nameBool = true
+    }
+}
+
+function checkAge() {
+    if (age.value < 14 || age.value > 30) {
+        errorAge.innerHTML = "Вы некорректно ввели возраст"
+        block.append(errorAge)
+    } else {
+        errorAge.innerHTML = ""
+        ageBool = true
+    }
+}
+
+function isStudent() {
+    if (select.value == "n") {
+        errorStudent.innerHTML = "Вход только для студентов"
+        block.append(errorStudent)
+    } else {
+        errorStudent.innerHTML = ""
+        studBool = true
+    }
+}
+function submitForm() {
+    checkName()
+    checkAge()
+    isStudent()
+    if (ageBool == true && nameBool == true && studBool == true) {
+        window.location.href = 'http://127.0.0.1:5500/calc.html'
+    }
+    
+}
